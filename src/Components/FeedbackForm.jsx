88942ec38c8b47c95b9392060918082a3e35e006
vohn237 <devohn99@outlook.com>
2024-10-03 11:27:25 -0500

    add form elements, implement state change for form data, implement change hadlers

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,17 +1,48 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    feedback: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
+      <nav>Tell Us What You Think</nav>
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <textarea
+          name="feedback"
+          placeholder="Your Feedback"
+          value={formData.feedback}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
